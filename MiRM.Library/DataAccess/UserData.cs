a8c5701c3722585e066dca5727a2bafa4d8f4905
oujunxu <oujun96@gmail.com>
2021-03-01 17:58:49 +0100

    Wired up the api, by creating an usercontroller which returns an usermodel containing the user data.

---
+++ MiRM.Library/DataAccess/UserData.cs
@@ -0,0 +1,23 @@
+﻿using MiRM.Library.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MiRM.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SQLDataAccess sql = new SQLDataAccess();
+            var p = new { Id = Id };
+           
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "MiRMDB");
+
+            return output;
+        }
+    }
+}
---
+++ MiRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using MiRM.Library.DataAccess;
+using MiRM.Library.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MiRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController: ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId(); // look up the user based on who they are.
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+
+    }
+}
